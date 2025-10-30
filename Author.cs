98fdc327b50245559c7d57de7158d85bdb33ee35
Alumno <Alumno@08SJLZ1P1103A11>
2025-10-29 19:44:20 -0500

    Agregar archivos de proyecto.

---
+++ Author.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miapp
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public Author(int authorId, string name)
+        {
+            AuthorId = authorId;
+            Name = name;
+        }
+
+        public static List<Author> Authors()
+        {
+            return new List<Author>
+            { 
+
+            new Author(1, "Miguel de Cervantes"),
+            new Author(2, "Charles Dickens"),
+            new Author(3, "J.R.R Tolkien"),
+            new Author(4, "Antonio"),
+            new Author(5, "Paulo Coelho"),
+            new Author(6, "Dan Brown"),
+            new Author(7, "Agatha Christie"),
+            new Author(8, "Cao Xuequin"),
+            new Author(9, "Mario Vargas Llosa"),
+            new Author(10, "Cesar Vallejo"),
+
+
+
+
+        };
+
+    }
+            
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using Miapp;
+
+
+
+var libros = Book.Books();
+var autores = Author.Authors();
+
+
+// mostrar por consola los 3 libros con mas ventas  
+
+Console.WriteLine("Los top 3 libros con ventas");
+var top3 = libros.OrderByDescending(l => l.Sales).Take(3);
+foreach (var l in top3)
+    Console.WriteLine($"{l.Title} - {l.Sales} millones");
+Separador();
+
+
+void Separador()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("--------------------------");
+    Console.ResetColor();
+}
+
+
+//mostrar por consola los 3 libros con menos ventas
+
+var menosVendidos = libros.OrderBy(l => l.Sales).Take(3);
+foreach (var l in menosVendidos)
+    Console.WriteLine($"{l.Title} - {l.Sales} millones");
+Separador();
+
+/*
+ Auhtor con mas publicaciones 
+ Agrupar los libros por su authorid
+ */
+
+var autortop = libros
+    .GroupBy(l => l.AuthorId) // agrupar por autor
+    .OrderByDescending(g => g.Count()) // ordena segun la cantidad de libros por author
+    .Join(autores,  //unidos por la lista de autores
+    g => g.Key,     //del grupo de libros usa el key de author id
+    a => a.AuthorId, //deñ autor usa su author
+    (g, a) => new {  //nombre del autor 
+        a.Name,     //nombre del autor
+        Cantidad = g.Count()    //cantidad de libros publicados 
+    })
+
+.First();
+
+
+Console.WriteLine($"{autortop.Name} - {autortop.Cantidad}Libros");
+
+//Libros hacemnos de 50 años publicados
+
+Console.WriteLine("Libros publicados hace menos de 50 años");
+
+int anioActual = DateTime.Now.Year;
+var recientes = libros.Where(l => (anioActual - l.PublicDate) < 50);
+foreach (var l in recientes)
+    Console.WriteLine($"{l.Title} - {l.PublicDate}");
+
+Separador();
+
+// libros que comience con "EL"
+
+Console.WriteLine("Libros que comienza con El");
+
+var autoresConEl = libros
+    .Where(l => l.Title.StartsWith("El"))
+    .Join(autores,
+    l => l.AuthorId,
+    a => a.AuthorId,
+    (l, a) => a.Name)
+    .Distinct();
+
+foreach (var nombre in autoresConEl)
+    Console.WriteLine(nombre);
+Separador();
